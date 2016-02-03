ca74bdf3e02959312e2129d4236cdb12afb24341
Nelio Alves <nelio.iftm@gmail.com>
2016-02-03 16:09:58 -0200

    Project CRUD somente list e carregamento de new (client-requestCriteria)

--- app/Repositories/ClientRepositoryEloquent.php
+++ app/Repositories/ClientRepositoryEloquent.php
@@ -2,10 +2,17 @@
 
 namespace CodeProject\Repositories;
 
-use CodeProject\Entities\Client;
+use Prettus\Repository\Criteria\RequestCriteria;
 use Prettus\Repository\Eloquent\BaseRepository;
 
+use CodeProject\Entities\Client;
+
 class ClientRepositoryEloquent extends BaseRepository implements ClientRepository {
+
+	protected $fieldSearchable = [
+		'name',
+		'email',
+	];
 
 	public function Model() {
 		return Client::class;
@@ -13,5 +20,9 @@ class ClientRepositoryEloquent extends BaseRepository implements ClientRepositor
 
     public function presenter() {
         return \CodeProject\Presenters\ClientPresenter::class;
+    }
+
+    public function boot() {
+        $this->pushCriteria(app(RequestCriteria::class));
     }
 }
